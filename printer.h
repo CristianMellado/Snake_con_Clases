0f8ffa1668736d7dfa919089fcbb8ec39e985b5d
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2021-11-21 18:51:42 -0500

    Update printer.h

--- printer.h
+++ printer.h
@@ -8,7 +8,7 @@ class Printer{
 		string img;
 
         Printer(int , string );
-        virtual ~Printer();    // usamos el virtual para que los hijos de esta clase puedan modificar esta función
+        virtual ~Printer();  // usamos el virtual para que los hijos de esta clase puedan modificar esta función
 		virtual bool same_pos(int, int);
 		string get_img();
 };
