ea8bb65d5f37b3996c9b52c62b3c2a7b253c8587
Kaori Miyazono <85631829+Marcelo1591@users.noreply.github.com>
2021-11-21 19:30:48 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,3 +1,15 @@
+/**
+Snake2.0
+
+
+Departamento de Ciencia de la Computación
+Prof: D.Sc. Manuel Eduardo Loaiza Fernández
+
+Alumnos:
+ - Cristian Mellado Baca
+ - Marcelo Torres Acuña
+ - Alexander Carpio Mamani
+*/
 #include "map.cpp"
 
 void run_game();  // prototipo para la función run_game.
