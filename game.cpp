39aa30a9a727b9358c7a100d860878cc0ce2d133
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2021-11-21 19:27:44 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,16 +6,16 @@ void run_game(){
 	
 	while(1){
 		do{
-			cout<<"\n Enter size(min 3 - max ?): ";cin>>size;
+			cout<<"\n Enter size(min 3 - max ?): ";cin>>size;     // validación de tamaño de mapa.
 		}while(size < 3);
 
-		Map *mapa = new Map(size);
+		Map *mapa = new Map(size);    // reservamos memoria para el objeto Map.
 		mapa->run_map();
 		
 		cout<<"\n Do you want to play again ? (yes: y / no: any key): ";cin>>op;
 		if (op != 'y') break;
 		
-		delete mapa;
+		delete mapa;    // liberamos la memoria del objeto mapa.
 	}
 	
 }
